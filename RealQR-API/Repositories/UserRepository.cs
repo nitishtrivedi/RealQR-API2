bd001a187c57e5de0a3a1a5999c572a5582b331c
nitishtrivedi <nitish_trivedi0194@yahoo.com>
2025-04-27 20:49:40 +0530

    Changes to edit user password logic

--- RealQR-API/Repositories/UserRepository.cs
+++ RealQR-API/Repositories/UserRepository.cs
@@ -28,6 +28,7 @@ namespace RealQR_API.Repositories
 
         public async Task<User> AddAsync(User user)
         {
+            user.PasswordHash = BCrypt.Net.BCrypt.HashPassword(user.PasswordHash);
             _dbContext.Users.Add(user);
             await _dbContext.SaveChangesAsync();
             return user;
@@ -38,19 +39,27 @@ namespace RealQR_API.Repositories
             if (id != user.Id) return false;
             var existingUser = await _dbContext.Users.FindAsync(id);
             if (existingUser == null) return false;
-            _dbContext.Entry(existingUser).CurrentValues.SetValues(user);
+
+            if (!string.IsNullOrWhiteSpace(user.PasswordHash))
+            {
+                existingUser.PasswordHash = BCrypt.Net.BCrypt.HashPassword(user.PasswordHash);
+            }
+
+            //_dbContext.Entry(existingUser).CurrentValues.SetValues(user);
+            // Update fields manually
+            existingUser.UserName = user.UserName;
+            existingUser.FirstName = user.FirstName;
+            existingUser.LastName = user.LastName;
+            existingUser.Email = user.Email;
+            existingUser.IsUserAdmin = user.IsUserAdmin;
+
+            _dbContext.Entry(existingUser).State = EntityState.Modified;
             await _dbContext.SaveChangesAsync();
             return true;
         }
 
         public async Task<bool> DeleteAsync(int id)
         {
-            //var user = await GetByIdAsync(id);
-            //if (user == null) return false;
-            //_dbContext.Users.Remove(user);
-            //await _dbContext.SaveChangesAsync();
-            //return true;
-
             var user = await _dbContext.Users
                 .Include(u => u.Enquiries)
                 .ThenInclude(e => e.EnquiryQuestionnaire)
